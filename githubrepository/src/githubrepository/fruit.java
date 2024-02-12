a18010e393b2318b3aac6b7833f52016497e864e
fatima <makandarfatima88@gmail.com>
2024-02-12 10:29:13 +0530

    commit

---
+++ githubrepository/src/githubrepository/fruit.java
@@ -0,0 +1,20 @@
+package githubrepository;
+
+public class fruit {
+	String color;
+	fruit(String color)
+	{
+		this.color=color;
+	}
+	public static void main(String[]args) {
+		//TODO Auto-generated method stub
+		fruit cherry=new fruit("green");
+		//String color=cherry.getcolor();
+		//System.out.println("color of cherry is"+color);
+	}
+	
+		
+	
+	
+
+}
